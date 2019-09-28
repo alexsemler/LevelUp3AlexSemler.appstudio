f5cb660eccea8d290c0d50b92720c73a12b70313
alexsemler <alex.semler03@gmail.com>
2019-09-28 11:39:12 -0500

    Version 1

--- forms/Payment/Payment.js
+++ forms/Payment/Payment.js
@@ -1,25 +1,28 @@
-function carLoanPayment(amount, months, interest) {
-    carPayment = amount*(interest(Math.pow(1+interest,months)))/(Math.pow(1+interest,months)-1)
-    return carPayment;
+function carLoanPayment(amt, mnt, rate) {
+    let newRate = (rate / 100 ) / 12
+    return (amt*(newRate * (Math.pow(1+newRate,mnt)))/(Math.pow(1+newRate,mnt)-1));
 }
 function homeLoanPayment(num1, num2, num3) {
 }
 
 let choice = prompt("Which calculator do you want to use? Car? Home? (use all lowercase) Enter stop to quit.")
 
-i = 0
-while (i = 0) {
+index = 0
+while (index == 0) {
     if (choice == "car") {
-    console.log("car")
-    let loanAmount = Number(prompt("What is the car loan amount? ex. 30000"))
-    let months = Number(prompt("How many months is the loan for? ex 30"))
-    let interest = Number(Prompt("How much is the interest? ex 5.7"))
-    print(`A car loan for ${loanAmount} over ${months} months at ${interest}% interest would have a monthly payment of ${carLoanPayment(loanAmount, months, interest)}`)
+        console.log("car")
+        let loanAmount = Number(prompt("What is the car loan amount? ex. 30000"))
+        let months = Number(prompt("How many months is the loan for? ex 30"))
+        let interest = Number(prompt("How much is the interest? ex 5.7"))
+        let payment = carLoanPayment(loanAmount, months, interest)
+        alert(`A car loan for ${loanAmount} over ${months} months at ${interest}% interest would have a monthly payment of ${payment}`)
+        let choice = prompt("Which calculator do you want to use? Car? Home? (use all lowercase) Enter stop to quit.")
     }       
     if (choice == "house") {
+    
     }
     if(choice == "stop") {
-    console.log("You stoped")
-    i += 1
+        console.log("You stoped")
+        index += 1
     }
 }
